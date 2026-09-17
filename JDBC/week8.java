fc0ba930b22afb12f63a6c03a74158784afba0df
vardhanvanga <vardhanvanga24@gmail.com>
2026-09-17 20:43:23 +0530

    Change database password to 'admin'

--- JDBC/week8.java
+++ JDBC/week8.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class week8 {
     static final String DB_URL = "jdbc:mysql://localhost:3306/training";
     static final String USER = "root";
-    static final String PASS = "Vardhan@09";
+    static final String PASS = "admin";
 
     public static void main(String args[]){
         
